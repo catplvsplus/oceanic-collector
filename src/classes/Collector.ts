d43d7e135b97f789b2ce115b3942e39f2b532a01
Ghex <69035887+NotGhex@users.noreply.github.com>
2023-03-16 22:33:57 +0800

    is this kowrect?

--- src/classes/Collector.ts
+++ src/classes/Collector.ts
@@ -2,6 +2,7 @@ import { Client, Collection, TypedEmitter } from 'oceanic.js';
 
 export interface CollectorOptions<Collected> {
     max?: number;
+    maxProcessed?: number;
     filter?: (collected: Collected) => boolean|Promise<boolean>;
     time?: number;
     idle?: number;
@@ -22,20 +23,56 @@ export interface CollectorEvents<Collected> {
 export abstract class Collector<Collected> extends TypedEmitter<CollectorEvents<Collected>> {
     private _timeout: NodeJS.Timeout|null = null;
     private _idleTimeout: NodeJS.Timeout|null = null;
+    private _endReason: string|null = null;
 
     readonly collection: Collection<string, Collected> = new Collection();
 
     public ended: boolean = false;
-    public endReason: string|null = null;
+    public received: number = 0;
     public lastCollectedTimestamp: number|null = null;
 
     get max() { return this.options.max; }
+    get maxProcessed() { return this.options.maxProcessed; }
     get filter() { return this.options.filter ?? (() => true); }
     get time() { return this.options.time; }
     get idle() { return this.options.idle; }
     get dispose() { return this.options.dispose; }
     get client() { return this.options.client; }
     get lastCollectedAt() { return this.lastCollectedTimestamp ? new Date(this.lastCollectedTimestamp) : null; }
+
+    get endReason() {
+        if (this.max && this.collection.size >= this.max) return 'limit';
+        if (this.maxProcessed && this.received >= this.maxProcessed) return 'processedLimit';
+
+        return this._endReason;
+    }
+
+    get next() {
+        return new Promise((res, rej) => {
+            if (this.ended) {
+                rej(this.collection);
+                return;
+            }
+
+            const cleanup = () => {
+                this.removeListener('collect', onCollect);
+                this.removeListener('end', onEnd);
+            };
+
+            const onCollect = (collected: Collected) => {
+                cleanup();
+                res(collected);
+            }
+
+            const onEnd = () => {
+                cleanup();
+                rej(this.collection);
+            }
+
+            this.once('collect', onCollect);
+            this.once('end', onEnd);
+        });
+    }
 
     constructor(readonly options: CollectorOptions<Collected>) {
         super();
@@ -47,7 +84,7 @@ export abstract class Collector<Collected> extends TypedEmitter<CollectorEvents<
         if (this.idle) this._idleTimeout = setTimeout(() => this.stop('idle'), this.idle).unref();
     }
 
-    public async handleCollect(collected: Collected): Promise<void> {
+    public async handleCollect(collected: any): Promise<void> {
         const collectedId = await this._collect(collected);
         if (!collectedId) return;
 
@@ -57,6 +94,7 @@ export abstract class Collector<Collected> extends TypedEmitter<CollectorEvents<
         }
 
         this.collection.set(collectedId, collected);
+        this.received++;
 
         this.emit('collect', collected);
         this.lastCollectedTimestamp = Date.now();
@@ -65,11 +103,11 @@ export abstract class Collector<Collected> extends TypedEmitter<CollectorEvents<
         this.checkEnd();
     }
 
-    public async handleDispose(disposed: Collected): Promise<void> {
+    public async handleDispose(disposed: any): Promise<void> {
         if (this.dispose !== false) return;
 
         const disposedId = await this._dispose(disposed);
-        if (!disposedId || !(await this.filter(disposed)) || !this.collection.has(disposedId)) return;
+        if (!disposedId || !this.collection.has(disposedId)) return;
 
         this.collection.delete(disposedId);
         this.emit('dispose', disposed);
@@ -89,7 +127,7 @@ export abstract class Collector<Collected> extends TypedEmitter<CollectorEvents<
             this._idleTimeout = null;
         }
 
-        this.endReason = reason ?? null;
+        this._endReason = reason ?? null;
         this.ended = true;
 
         this.emit('end', this.collection, this.endReason);
@@ -116,6 +154,10 @@ export abstract class Collector<Collected> extends TypedEmitter<CollectorEvents<
 
         clearTimeout(this._idleTimeout);
         this._idleTimeout = setTimeout(() => this.stop('idle'), idle ?? this.idle).unref();
+    }
+
+    public toArray(): Collected[] {
+        return this.collection.toArray();
     }
 
     protected abstract _collect(collected: Collected): Promise<string|null>;
---
+++ src/classes/MessageCollector.ts
@@ -0,0 +1,63 @@
+import { AnyGuildChannelWithoutThreads, AnyTextChannel, ChannelTypes, DeletedPrivateChannel, Guild, Message, PossiblyUncachedMessage, PossiblyUncachedThread, PrivateChannel, TextableChannel, Uncached } from 'oceanic.js';
+import { Collector, CollectorOptions } from './Collector';
+
+export interface MessageCollectorOptions extends CollectorOptions<Message> {
+    channel: AnyTextChannel;
+}
+
+export class MessageCollector extends Collector<Message> {
+    readonly channel: AnyTextChannel;
+
+    constructor(options: MessageCollectorOptions) {
+        super(options);
+
+        this.channel = options.channel;
+
+        this._handleMessageDeleteBulk = this._handleMessageDeleteBulk.bind(this);
+        this._handleChannelDelete = this._handleChannelDelete.bind(this);
+        this._handleThreadDelete = this._handleThreadDelete.bind(this);
+        this._handleGuildDelete = this._handleGuildDelete.bind(this);
+
+        this.client.on('messageCreate', this.handleCollect);
+        this.client.on('messageDelete', this.handleDispose);
+        this.client.on('messageDeleteBulk', this._handleMessageDeleteBulk);
+        this.client.on('channelDelete', this._handleChannelDelete);
+        this.client.on('threadDelete', this._handleThreadDelete);
+        this.client.on('guildDelete', this._handleGuildDelete);
+
+        this.once('end', () => {
+            this.client.removeListener('messageCreate', this.handleCollect);
+            this.client.removeListener('messageDelete', this.handleDispose);
+            this.client.removeListener('messageDeleteBulk', this._handleMessageDeleteBulk);
+            this.client.removeListener('channelDelete', this._handleChannelDelete);
+            this.client.removeListener('threadDelete', this._handleThreadDelete);
+            this.client.removeListener('guildDelete', this._handleGuildDelete);
+        });
+    }
+
+    protected async _handleMessageDeleteBulk(messages: PossiblyUncachedMessage[]): Promise<void> {
+        for (const message of messages) {
+            this.handleDispose(message);
+        }
+    }
+
+    protected async _handleChannelDelete(channel: AnyGuildChannelWithoutThreads|PrivateChannel|DeletedPrivateChannel): Promise<void> {
+        if (channel.id === this.channel.id) this.stop('channelDelete');
+    }
+
+    protected async _handleThreadDelete(thread: PossiblyUncachedThread): Promise<void> {
+        if (thread.id === this.channel.id) this.stop('threadDelete');
+    }
+
+    protected async _handleGuildDelete(guild: Uncached|Guild): Promise<void> {
+        if (!(this.channel.type === ChannelTypes.DM || this.channel.type === ChannelTypes.GROUP_DM) && guild.id === this.channel.guildID) this.stop('guildDelete');
+    }
+
+    protected async _collect(collected: Message): Promise<string | null> {
+        return collected.channelID === this.channel.id ? collected.id : null;
+    }
+
+    protected async _dispose(collected: PossiblyUncachedMessage): Promise<string | null> {
+        return collected.channelID === this.channel.id ? collected.id : null;
+    }
+}
